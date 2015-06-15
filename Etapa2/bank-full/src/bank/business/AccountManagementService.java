10850f53e30d936dce6f59c0bfc617b4172ca651
barros-jf <jfbarros@inf.ufrgs.br>
2015-06-15 12:50:22 -0300

    Etapa2 reference folder

---
+++ Etapa2/bank-full/src/bank/business/AccountManagementService.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 16 Dec 2013 16:17:23
+ */
+package bank.business;
+
+import java.util.Date;
+import java.util.List;
+
+import bank.business.domain.CurrentAccount;
+import bank.business.domain.Employee;
+import bank.business.domain.Transfer;
+
+/**
+ * @author Ingrid Nunes
+ * 
+ */
+public interface AccountManagementService {
+
+	public CurrentAccount createCurrentAccount(long branch, String name,
+			String lastName, int cpf, Date birthday, double balance)
+			throws BusinessException;
+
+	public Employee login(String username, String password)
+			throws BusinessException;
+	
+	public List<Transfer> getAllTranfersToCheck()
+			throws BusinessException;
+	
+	public Transfer authorizeTransfer(Transfer transfer) throws BusinessException;
+	
+	public Transfer cancelTransfer(Transfer transfer) throws BusinessException;
+
+}
